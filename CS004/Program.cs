7b1e7ec77aedf25f995af6bb060dd955aa636266
huongcucnguyen <huongcucnguyen0889@gmail.com>
2024-04-01 21:25:48 +0700

    Add CS004

---
+++ CS004/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+namespace Cs004
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //bool isOnline;
+            // == > < >= <= !=
+            // isOnline = true;
+            // isOnline = false;
+            // Console.WriteLine(isOnline);
+
+            // bool kq;
+            // int a = 5;
+            // int b = 6;
+
+            // kq = a == b;
+
+            // Console.WriteLine("a == b ==> {0}", kq);
+            // Console.WriteLine($"a != b ==> {a != b}");
+            // Console.WriteLine($"a > b ==> {a > b}");
+            // Console.WriteLine($"a >= b ==> {a >= b}");
+            // Console.WriteLine($"a < b ==> {a < b}");
+            // Console.WriteLine($"a <= b ==> {a <= b}");
+
+            bool kq;
+            // && || !
+            // bool a = false;
+            bool a = true;
+            //bool b = true;
+            bool b = false;
+
+            //kq = a && b;
+            kq = !(a || b);
+
+            // kq = ! a;
+
+            Console.WriteLine(!kq);
+        }
+    }
+}
