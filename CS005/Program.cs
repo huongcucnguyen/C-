78bfbbd3bca57e33e74a665ad837d957cdc5e202
Archway-Project <archway-project@Huong-Cuc.local>
2024-04-02 17:01:33 +0700

    Add CS005

---
+++ CS005/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+
+namespace CS005
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /*
+                if (dieu_kien_logic)
+                dong_lenh;
+
+                if (dieu_kien_logic)
+                {
+                    ...
+                }
+                else if (dieu_kien_1)
+                {
+                    ...khoi lenh else
+                }
+                else if (dieu_kien_2)
+                {
+                    ...khoi lenh else
+                }
+                else
+                {
+
+                }
+            */
+
+            // int a;
+            // Console.WriteLine("Nhap so nguyen a:");
+            // a = int.Parse(Console.ReadLine());
+
+            // //a % 2  == 0
+            // if (a % 2 == 0)
+            // {
+            //     Console.WriteLine("Ket qua:");
+            //     Console.WriteLine($"So {a} la so chan");
+            // }
+            // else
+            // {
+            //     Console.WriteLine("Ket qua:");
+            //     Console.WriteLine($"So {a} la so le");
+            // }
+                
+            // Console.WriteLine("The end");
+
+            /*
+            [8-10] : gioi
+            [6.5-8] : kha
+            [0 -5.0] : yeu
+            */
+        }
+    }
+}
